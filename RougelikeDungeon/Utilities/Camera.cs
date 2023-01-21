85cc48346302ef8c325c29edbac56bf0f0511f63
Alex Eckardt <realalexeck@gmail.com>
2023-01-20 23:12:03 -0500

    Camera Into Singleton

--- RougelikeDungeon/Utilities/Camera.cs
+++ RougelikeDungeon/Utilities/Camera.cs
@@ -13,18 +13,18 @@ using Microsoft.Xna.Framework.Media;
 
 namespace RougelikeDungeon.Utilities
 {
-    static class Camera
+    public class Camera
     {
-        static private Matrix transformMatrix; //A transformation matrix containing info on our position, how much we are rotated and zoomed etc.
-        static private Vector2 position;
-        static public float rotation;
-        static private float zoom;
-        static private Rectangle screenRect;
+        private Matrix transformMatrix; //A transformation matrix containing info on our position, how much we are rotated and zoomed etc.
+        private Vector2 position;
+        public float rotation;
+        private float zoom;
+        private Rectangle screenRect;
        
-        static public bool updateYAxis = false; //Should the camera move along on the y axis?
-        static public bool updateXAxis = true; //Should the camera move along on the x axis?
+        public bool updateYAxis = false; //Should the camera move along on the y axis?
+        public bool updateXAxis = true; //Should the camera move along on the x axis?
 
-        public static void Initialize()
+        private Camera()
         {
             zoom = 1.0f;
             rotation = 0.0f;
@@ -33,15 +33,27 @@ namespace RougelikeDungeon.Utilities
             position = new Vector2(Resolution.VirtualWidth / 2, Resolution.VirtualHeight / 2);
         }
 
+        private static Camera inst;
+        public static Camera Instance
+        {
+            get
+            {
+                if (inst == null)
+                    inst = new Camera();
+
+                return inst;
+            }
+        }
+
         /// <summary>
         /// This rectangle covers the entire screen based on where the camera is, useful if you need to determine what is currently viewable to the player, or the position of the camera.
         /// </summary>
-        public static Rectangle ScreenRect 
+        public Rectangle ScreenRect 
         {
             get { return screenRect; }
         }
 
-        public static float Zoom
+        public float Zoom
         {
             get { return zoom; }
             set
@@ -50,13 +62,13 @@ namespace RougelikeDungeon.Utilities
             }
         }
 
-        public static void Update(Vector2 follow)
+        public void Update(Vector2 follow)
         {
             UpdateMovement(follow);
             CalculateMatrixAndRectangle();
         }
 
-        private static void UpdateMovement(Vector2 follow)
+        private void UpdateMovement(Vector2 follow)
         {
             //Make the camera center on and follow the position:
             if (updateXAxis == true)
@@ -69,7 +81,7 @@ namespace RougelikeDungeon.Utilities
         /// <summary>
         /// Immediately sets the camera to look at the position passed in.
         /// </summary>
-        public static void LookAt(Vector2 lookAt)
+        public void LookAt(Vector2 lookAt)
         {
             //Immediately looks at the vector passed in:
             if (updateXAxis == true)
@@ -78,7 +90,7 @@ namespace RougelikeDungeon.Utilities
                 position.Y = lookAt.Y;
         }
 
-        private static void CalculateMatrixAndRectangle()
+        private void CalculateMatrixAndRectangle()
         {
             //The math involved with calculated our transformMatrix and screenRect is a little intense, so instead of calling the math whenever we need these variables,
             //we'll calculate them once each frame and store them... when someone needs these variables we will simply return the stored variable instead of re cauclating them every time.
@@ -102,7 +114,7 @@ namespace RougelikeDungeon.Utilities
         /// <summary>
         /// Calculates the screenRect based on where the camera currently is.
         /// </summary>
-        private static Rectangle VisibleArea()
+        private Rectangle VisibleArea()
         {
             Matrix inverseViewMatrix = Matrix.Invert(transformMatrix);
             Vector2 tl = Vector2.Transform(Vector2.Zero, inverseViewMatrix);
@@ -118,7 +130,7 @@ namespace RougelikeDungeon.Utilities
             return new Rectangle((int)min.X, (int)min.Y, (int)(Resolution.VirtualWidth / zoom), (int)(Resolution.VirtualHeight / zoom));
         }
 
-        public static Matrix GetTransformMatrix()
+        public Matrix GetTransformMatrix()
         {
             return transformMatrix; //Return the transformMatrix we calculated earlier in this frame.
         }
